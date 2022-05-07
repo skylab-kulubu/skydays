362fb59f59d5758f26b9008a8f1fdc564389024e
Emirhan Karakadıoğlu <70448242+Bolzac@users.noreply.github.com>
2022-05-07 16:51:09 +0300

    Add files via upload

--- agc/script.js
+++ agc/script.js
@@ -127,3 +127,68 @@ contObserver.observe(container);
 contObserver.observe(informationContainer);
 /*titleObserver.observe(overviewTitle);*/
 /*titleObserver.observe(descriptionGift);*/
+
+class Guest {
+  constructor(name, status, about, company, imgSource) {
+    this.name = name;
+    this.status = status;
+    this.about = about;
+    this.company = company;
+    this.imgSource = imgSource ? imgSource : './sources/blank-profile-picture-973460_960_720.png'
+  }
+}
+
+const guests28 = [
+  new Guest('*', '*', '*', '*'),
+  new Guest('Gökay Pekşen', 'Principal Advisor', 'Kariyer Planlaması', 'Prime Threat', './sources/Gökay-Pekşen.jpg'),
+  new Guest('*', '*', '*', '*'),
+  new Guest('*', '*', '*', '*'),
+  new Guest('*', '*', '*', '*')
+]
+
+const guests29 = [
+  new Guest('Osman Doğan', 'CEO & FOUNDER', 'CAR HACKING', 'GAIS', './sources/Osman-Doğan.jpg'),
+  new Guest('PERİM ASENA POLAT', 'CYBER SECURITY CONSULTANT', 'WEB SECURITY', 'DELOITTE', './sources/Perim-Asena-Polat.jpg'),
+  new Guest('*', '*', '*', '*'),
+  new Guest('*', '*', '*', '*'),
+  new Guest('*', '*', '*', '*'),
+  new Guest('*', '*', '*', '*')
+]
+
+let innerhtml = '';
+guests28.map((guest, index) => {
+  innerhtml = innerhtml.concat(`
+    <div class="card" style="width: 25rem;">
+      <img src=${guest.imgSource} class="card-img-top" alt="...">
+      <div class="card-body">
+        <h2 class="card-title">${guest.name}</h2>
+      </div>
+    </div>
+  `)
+  if (index === 2) {
+    window.document.getElementById("activity-row-1").innerHTML = innerhtml
+    innerhtml = '';
+  }
+  if (index === 4) {
+    window.document.getElementById("activity-row-2").innerHTML = innerhtml
+    innerhtml = '';
+  }
+})
+guests29.map((guest, index) => {
+  innerhtml = innerhtml.concat(`
+    <div class="card" style="width: 25rem;">
+      <img src=${guest.imgSource} class="card-img-top" alt="...">
+      <div class="card-body">
+        <h2 class="card-title">${guest.name}</h2>
+      </div>
+    </div>
+  `)
+  if (index === 2) {
+    window.document.getElementById("activity-row-3").innerHTML = innerhtml
+    innerhtml = '';
+  }
+  if (index === 5) {
+    window.document.getElementById("activity-row-4").innerHTML = innerhtml
+    innerhtml = '';
+  }
+})
